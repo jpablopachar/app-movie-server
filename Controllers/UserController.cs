57dc50478daa7733930d93a25634d74f4db740e0
jpablopachar <jpablopachar1993@gmail.com>
2025-05-24 00:38:09 -0500

    Agregar métodos para registrar y autenticar usuarios en UserController, incluyendo documentación detallada.

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -18,6 +18,15 @@ namespace app_movie_server.Controllers
         private readonly IUserRepository _userRepository = userRepository;
         private readonly IMapper _mapper = mapper;
 
+        /// <summary>
+        /// Obtiene una lista de todos los usuarios registrados en el sistema.
+        /// </summary>
+        /// <returns>Una respuesta <see cref="IActionResult"/> que contiene la lista de usuarios.</returns>
+        /// <response code="200">Lista de usuarios obtenida exitosamente.</response>
+        /// <response code="403">Acceso denegado. Se requiere autorización.</response>
+        /// <remarks>
+        /// Este método requiere autorización y solo puede ser accedido por usuarios con el rol de "Admin".
+        /// </remarks>
         [Authorize(Roles = "Admin")]
         [HttpGet]
         [ResponseCache(CacheProfileName = "Default30Seconds")]
@@ -38,6 +47,18 @@ namespace app_movie_server.Controllers
             return Ok(usersDto);
         }
 
+        /// <summary>
+        /// Obtiene la información de un usuario específico utilizando su ID.
+        /// </summary>
+        /// <param name="userId">ID del usuario a obtener.</param>
+        /// <returns>Una respuesta <see cref="IActionResult"/> que indica el resultado de la operación.</returns>
+        /// <response code="200">Información del usuario obtenida exitosamente.</response>
+        /// <response code="400">El ID del usuario es inválido o faltan datos requeridos.</response>
+        /// <response code="403">Acceso denegado. Se requiere autorización.</response>
+        /// <response code="404">El usuario no fue encontrado.</response>
+        /// <remarks>
+        /// Este método requiere autorización y solo puede ser accedido por usuarios con el rol de "Admin".
+        /// </remarks>
         [Authorize(Roles = "Admin")]
         [HttpGet("{userId}", Name = "GetUser")]
         [ResponseCache(CacheProfileName = "Default30Seconds")]
@@ -59,6 +80,21 @@ namespace app_movie_server.Controllers
             return Ok(userDto);
         }
 
+        /// <summary>
+        /// Registra un nuevo usuario en el sistema utilizando las credenciales proporcionadas.
+        /// </summary>
+        /// <param name="registerDto">
+        /// Objeto <see cref="RegisterDto"/> que contiene la información necesaria para registrar el usuario.
+        /// </param>
+        /// <returns>
+        /// Una respuesta <see cref="IActionResult"/> que indica el resultado de la operación de registro.
+        /// </returns>
+        /// <response code="201">Registro exitoso del nuevo usuario.</response>
+        /// <response code="400">El nombre de usuario ya existe o faltan datos requeridos.</response>
+        /// <response code="500">Error interno del servidor durante el proceso de registro.</response>
+        /// <remarks>
+        /// Este método permite el acceso anónimo. Valida que el nombre de usuario sea único antes de proceder con el registro.
+        /// </remarks>
         [AllowAnonymous]
         [HttpPost("register")]
         [ProducesResponseType(StatusCodes.Status201Created)]
@@ -96,6 +132,24 @@ namespace app_movie_server.Controllers
             return Ok(_apiResponse);
         }
 
+        /// <summary>
+        /// Inicia sesión de un usuario utilizando las credenciales proporcionadas.
+        /// </summary>
+        /// <param name="loginDto">
+        /// Objeto <see cref="LoginDto"/> que contiene las credenciales del usuario para autenticación.
+        /// </param>
+        /// <returns>
+        /// Una respuesta <see cref="IActionResult"/> que indica el resultado de la operación de inicio de sesión.
+        /// Devuelve <see cref="OkObjectResult"/> con la información del usuario y el token si la autenticación es exitosa,
+        /// o <see cref="BadRequestObjectResult"/> si las credenciales son inválidas.
+        /// </returns>
+        /// <response code="200">Inicio de sesión exitoso. Se devuelve el usuario autenticado y el token.</response>
+        /// <response code="400">Las credenciales proporcionadas son inválidas o faltan datos requeridos.</response>
+        /// <response code="500">Error interno del servidor durante el proceso de autenticación.</response>
+        /// <remarks>
+        /// Este método permite a usuarios anónimos autenticarse en el sistema. Si las credenciales son correctas,
+        /// se genera un token de autenticación y se retorna junto con la información del usuario.
+        /// </remarks>
         [AllowAnonymous]
         [HttpPost("login")]
         [ProducesResponseType(StatusCodes.Status200OK)]
