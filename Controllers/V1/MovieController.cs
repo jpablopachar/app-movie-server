1b471bfe464dc4f113d42f481c5f8e46e3e316df
jpablopachar <jpablopachar1993@gmail.com>
2025-05-26 03:49:02 -0500

    Agregar métodos para obtener, crear, actualizar y eliminar películas en MovieController, incluyendo manejo de imágenes y búsqueda por categoría.

--- Controllers/V1/MovieController.cs
+++ Controllers/V1/MovieController.cs
@@ -1,7 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using app_movie_server.Models;
 using app_movie_server.Models.Dtos;
 using app_movie_server.Repositories.Interfaces;
 using AutoMapper;
@@ -43,6 +40,223 @@ namespace app_movie_server.Controllers.V1
                 };
 
                 return Ok(response);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Se ha producido un error en el servidor.");
+            }
+        }
+
+        [AllowAnonymous]
+        [HttpGet("{movieId:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult GetMovieById(int movieId)
+        {
+            var movie = _movieRepository.GetMovie(movieId);
+
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            var movieDto = _mapper.Map<MovieDto>(movie);
+
+            return Ok(movieDto);
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpPost]
+        [ProducesResponseType(201, Type = typeof(MovieDto))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public IActionResult CreateMovie([FromBody] CreateMovieDto createMovieDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (createMovieDto == null)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (_movieRepository.MovieExists(createMovieDto.Name!))
+            {
+                ModelState.AddModelError("", "La película ya existe.");
+
+                return BadRequest(ModelState);
+            }
+
+            var movie = _mapper.Map<Movie>(createMovieDto);
+
+            if (createMovieDto.Image != null)
+            {
+                string fileName = movie.Id + System.Guid.NewGuid().ToString() + Path.GetExtension(createMovieDto.Image.FileName);
+                string filaPath = @"wwwroot\images\" + fileName;
+                var directoryLocation = Path.Combine(Directory.GetCurrentDirectory(), filaPath);
+                FileInfo file = new(directoryLocation);
+
+                if (file.Exists)
+                {
+                    file.Delete();
+                }
+
+                using (var stream = new FileStream(directoryLocation, FileMode.Create))
+                {
+                    createMovieDto.Image.CopyTo(stream);
+                }
+
+                var baseUrl = $"{HttpContext.Request.Scheme}://{HttpContext.Request.Host.Value}{HttpContext.Request.PathBase.Value}";
+
+                movie.ImagePath = $"{baseUrl}/images/{fileName}";
+
+                movie.ImageLocalPath = filaPath;
+            }
+            else
+            {
+                movie.ImagePath = "https://placehold.co/600x400";
+            }
+
+            _movieRepository.CreateMovie(movie);
+
+            return CreatedAtRoute("", new { movieId = movie.Id }, movie);
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpPut("{movieId:int}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public IActionResult UpdateMovie(int movieId, [FromBody] UpdateMovieDto updateMovieDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (updateMovieDto == null || movieId != updateMovieDto.Id)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var existingMovie = _movieRepository.GetMovie(movieId);
+
+            if (existingMovie == null)
+            {
+                return NotFound($"No se encontró la película con el ID {movieId}.");
+            }
+
+            var movie = _mapper.Map<Movie>(updateMovieDto);
+
+            if (updateMovieDto.Image != null)
+            {
+                string fileName = movie.Id + System.Guid.NewGuid().ToString() + Path.GetExtension(updateMovieDto.Image.FileName);
+                string filaPath = @"wwwroot\images\" + fileName;
+                var directoryLocation = Path.Combine(Directory.GetCurrentDirectory(), filaPath);
+                FileInfo file = new(directoryLocation);
+
+                if (file.Exists)
+                {
+                    file.Delete();
+                }
+
+                using (var stream = new FileStream(directoryLocation, FileMode.Create))
+                {
+                    updateMovieDto.Image.CopyTo(stream);
+                }
+
+                var baseUrl = $"{HttpContext.Request.Scheme}://{HttpContext.Request.Host.Value}{HttpContext.Request.PathBase.Value}";
+
+                movie.ImagePath = $"{baseUrl}/images/{fileName}";
+
+                movie.ImageLocalPath = filaPath;
+            }
+            else
+            {
+                movie.ImagePath = "https://placehold.co/600x400";
+            }
+
+            _movieRepository.UpdateMovie(movie);
+
+            return NoContent();
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpDelete("{movieId:int}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public IActionResult DeleteMovie(int movieId)
+        {
+            if (!_movieRepository.MovieExists(movieId))
+            {
+                return NotFound("");
+            }
+
+            var movie = _movieRepository.GetMovie(movieId);
+
+            if (!_movieRepository.DeleteMovie(movie))
+            {
+                ModelState.AddModelError("", $"No se pudo eliminar la película con el ID {movieId}.");
+
+                return StatusCode(StatusCodes.Status500InternalServerError, ModelState);
+            }
+
+            return NoContent();
+        }
+
+        [AllowAnonymous]
+        [HttpGet("GetMoviesByCategory/{categoryId:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public IActionResult GetMoviesByCategory(int categoryId)
+        {
+            try
+            {
+                var movies = _movieRepository.GetMoviesByCategory(categoryId);
+
+                if (movies == null || movies.Count == 0)
+                {
+                    return NotFound("No se encontraron películas para esta categoría.");
+                }
+
+                var moviesDto = movies.Select(movie => _mapper.Map<MovieDto>(movie)).ToList();
+
+                return Ok(moviesDto);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Se ha producido un error en el servidor.");
+            }
+        }
+
+        [AllowAnonymous]
+        [HttpGet("Search")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public IActionResult SearchMovies(string name)
+        {
+            try
+            {
+                var movies = _movieRepository.SearchMovie(name);
+
+                if (!movies.Any())
+                {
+                    return NotFound("No se encontraron películas con ese nombre.");
+                }
+
+                var moviesDto = _mapper.Map<IEnumerable<MovieDto>>(movies);
+
+                return Ok(moviesDto);
             }
             catch (Exception)
             {
